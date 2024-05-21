7e1880b713050517819b892d99b98577c2489def
Erick <erickperez091@hotmail.com>
2024-05-21 17:45:41 -0600

    migrate to spring boot 3.2.5

--- src/main/java/com/example/products/ProductApp.java
+++ src/main/java/com/example/products/ProductApp.java
@@ -2,7 +2,7 @@ package com.example.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -11,7 +11,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 @ComponentScan( { "com.example" } )
 @EnableJpaRepositories( basePackages = { "com.example.products.repository" } )
 @EnableKafka
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ProductApp {
     public static void main( String[] args ) {
         SpringApplication.run( ProductApp.class, args );
--- src/main/java/com/example/products/repository/impl/CustomRepositoryImpl.java
+++ src/main/java/com/example/products/repository/impl/CustomRepositoryImpl.java
@@ -6,9 +6,9 @@ import com.example.products.repository.CustomRepository;
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
